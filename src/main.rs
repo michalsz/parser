487497a91b63902f28ae0f38398eae370d4d33d7
Michal Szymanski <michal.szymanski@railwaymen.org>
2023-03-09 15:05:58 +0100

    Output as file with founds

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 // use std::io;
+extern crate exitcode;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
 	let pattern = std::env::args().nth(1).expect("no pattern given");
 	let path = std::env::args().nth(2).expect("no path given");
 	
-	println!("{}", pattern);
+	println!("search in the file: {}", pattern);
 	
-	if pattern == "ok" {
-		let content = std::fs::read_to_string(path).expect("could not read file");
-		for line in content.lines() {
-		    if line.contains(&pattern) {
-		               println!("{}", line);
-           }
-		}
+	let content = std::fs::read_to_string(path).expect("could not read file");
+	
+	
+	let mut contents = String::new();
+	for line in content.lines() {
+	    if line.contains(&pattern) {
+			contents += line;
+			contents += "\n";
+        }
 	}
+
+	let mut file = File::create("output.txt")
+	               .expect("Error encountered while creating file!");
+	
+	file.write_all(contents.as_bytes()).expect("Error encountered while creating file!");
+	
+	println!("Done");
+	std::process::exit(exitcode::OK);
 }
