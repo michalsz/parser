273fb1ab01b45ca6d7887b0d4c9b1453455bc6b6
Michal Szymanski <michal.szymanski@railwaymen.org>
2023-03-26 12:09:45 +0200

    Struct for Finder

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,54 @@
 extern crate exitcode;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
+
+struct Finder{
+	pattern: String,
+	file_path: String
+}
+
+impl Finder{
+	fn search(&self) {
+		println!("Search in the file: {}", self.pattern);
+		
+		let content = std::fs::read_to_string(&self.file_path).expect("could not read file");
+		
+		let mut contents = String::new();
+		for line in content.lines() {
+		    if line.contains(&self.pattern) {
+				contents += line;
+				contents += "\n";
+		        }
+		}
+				
+		let mut file = self.create_file();
+		file.write_all(contents.as_bytes()).expect("Error encountered while creating file!");
+	}
+	
+	fn create_file(&self) -> File {
+		let mut file_name = "output.txt";
+
+	    if Path::new(file_name).exists() {
+	        println!("File exists");
+			file_name = "output_1.txt";
+	    }
+	    else{
+	        println!("File does not exist");
+	    }  
+				
+	   let file = File::create(file_name)
+		               .expect("Error encountered while creating file!");
+	   file 
+	}
+}
 
 fn main() {
-	let pattern = std::env::args().nth(1).expect("no pattern given");
-	let path = std::env::args().nth(2).expect("no path given");
+	let finder = Finder{pattern: std::env::args().nth(1).expect("no pattern given"), 
+					    file_path: std::env::args().nth(2).expect("no path given")};
 	
-	println!("search in the file: {}", pattern);
-	
-	let content = std::fs::read_to_string(path).expect("could not read file");
-	
-	
-	let mut contents = String::new();
-	for line in content.lines() {
-	    if line.contains(&pattern) {
-			contents += line;
-			contents += "\n";
-        }
-	}
-
-	let mut file = File::create("output.txt")
-	               .expect("Error encountered while creating file!");
-	
-	file.write_all(contents.as_bytes()).expect("Error encountered while creating file!");
-	
+	finder.search();
+		
 	println!("Done");
 	std::process::exit(exitcode::OK);
 }
