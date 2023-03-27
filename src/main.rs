39a016a293ac365a72a164bc347d47450b54e5bf
Michal Szymanski <michal.szymanski@railwaymen.org>
2023-03-27 08:38:09 +0200

    Refactored

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ use std::path::Path;
 
 struct Finder{
 	pattern: String,
-	file_path: String
+	file_path: String,
+	output_file_name: String
 }
 
 impl Finder{
-	fn search(&self) {
+	fn search(self) {
 		println!("Search in the file: {}", self.pattern);
 		
 		let content = std::fs::read_to_string(&self.file_path).expect("could not read file");
@@ -22,31 +23,31 @@ impl Finder{
 				contents += "\n";
 		        }
 		}
+		println!("{}", std::mem::size_of_val(&contents).to_string());
 				
 		let mut file = self.create_file();
 		file.write_all(contents.as_bytes()).expect("Error encountered while creating file!");
 	}
 	
-	fn create_file(&self) -> File {
-		let mut file_name = "output.txt";
-
-	    if Path::new(file_name).exists() {
+	fn create_file(mut self) -> File {
+	    if Path::new(&self.output_file_name).exists() {
 	        println!("File exists");
-			file_name = "output_1.txt";
-	    }
-	    else{
+			self.output_file_name = "output_1.txt".to_string();
+	    } else{
 	        println!("File does not exist");
 	    }  
 				
-	   let file = File::create(file_name)
+	   let file = File::create(self.output_file_name)
 		               .expect("Error encountered while creating file!");
 	   file 
 	}
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
 	let finder = Finder{pattern: std::env::args().nth(1).expect("no pattern given"), 
-					    file_path: std::env::args().nth(2).expect("no path given")};
+					    file_path: std::env::args().nth(2).expect("no path given"),
+						output_file_name: String::from("output.txt")
+						};
 	
 	finder.search();
 		
